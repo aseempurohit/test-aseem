d3e89886d976ba1854565bcf182145c0954ece67
aseempurohit <purohitaseem@gmail.com>
2018-02-22 15:31:08 +0530

    Update service-time.py

--- service-time.py
+++ service-time.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 import sys
 import time
+from pytz import reference
+import datetime
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,8 +13,34 @@ class TodoSimple(Resource):
     def get(self):
         return { 'time' : time.strftime("%b %d %Y %H:%M:%S") }
 
-api.add_resource(TodoSimple, '/time')
+class TodoSimple1(Resource):
+    def get(self):
+        localtime = reference.LocalTimezone()
+        today = datetime.datetime.now()
+        return { 'timezone' : localtime.tzname(today) }
 
+class TodoYear(Resource):
+    def get(self):
+        now = datetime.datetime.now()
+        return { 'date' : now.year}
+
+
+class TodoMonth(Resource):
+    def get(self):
+        now = datetime.datetime.now()
+        return { 'date' : now.month}
+
+
+class TodoDay(Resource):
+    def get(self):
+        now = datetime.datetime.now()
+        return { 'date' : now.day}
+
+api.add_resource(TodoSimple, '/time')
+api.add_resource(TodoSimple1, '/timezone')
+api.add_resource(TodoYear, '/year')
+api.add_resource(TodoMonth, '/month')
+api.add_resource(TodoDay, '/day')
 
 if __name__ == '__main__':
     if(len(sys.argv) > 1):
